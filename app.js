a2b9470b805a7f07d616be2f98791a220b8b5364
Souma <souma.hitech@gmail.com>
2020-08-03 03:07:19 +0530

    complete design compose page and a route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 let paragraphContent = "";
+let posts = [];
 
 const app = express();
 
@@ -34,6 +35,24 @@ app.get("/contact",function(req,res){
   paragraphContent = contactContent;
   res.render("contact",{paragraph:paragraphContent});
 });
+
+// compose route get request 
+app.get("/compose",function(req,res){
+  res.render("compose");
+});
+
+// compose post request
+app.post("/compose",function(req,res){
+  const post = {
+    title:req.body.postTitle.trim(),
+    content:req.body.postBody.trim()
+  };
+  posts.push(post);
+  console.log(posts);
+  res.redirect("/");
+  
+});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
